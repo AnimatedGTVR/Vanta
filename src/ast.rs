3428fec7ed0a8326e2a18e9d067192c5b9eaf5cc
Animated <jaxsonjerryb@gmail.com>
2026-09-05 18:39:30 -0400

    Add src/ast.rs for Rust interpreter bootstrap

---
+++ src/ast.rs
@@ -0,0 +1,42 @@
+#[derive(Debug, Clone, PartialEq)]
+pub struct Program { pub module: String, pub functions: Vec<Function> }
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Function {
+    pub name: String,
+    pub parameters: Vec<Parameter>,
+    pub return_type: Type,
+    pub body: Vec<Statement>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Parameter { pub name: String, pub mutable: bool, pub ty: Type }
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Type { Int, Bool, String, Void }
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Statement {
+    Bind { name: String, mutable: bool, ty: Option<Type>, value: Expression },
+    Assign { name: String, value: Expression },
+    Return(Option<Expression>),
+    If { condition: Expression, then_body: Vec<Statement>, else_body: Vec<Statement> },
+    Expression(Expression),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expression {
+    Integer(i64),
+    Bool(bool),
+    String(String),
+    Variable(String),
+    Unary { operator: UnaryOperator, operand: Box<Expression> },
+    Binary { left: Box<Expression>, operator: BinaryOperator, right: Box<Expression> },
+    Call { name: String, arguments: Vec<Expression> },
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum UnaryOperator { Negate, Not }
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum BinaryOperator { Add, Subtract, Multiply, Divide, Equal, NotEqual, Less, LessEqual, Greater, GreaterEqual }
